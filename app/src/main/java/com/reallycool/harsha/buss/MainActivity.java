3c39273a6ad83b471134fe56b87c7386b5731f0e
Harsha <9515246+harsha-main@users.noreply.github.com>
2017-12-31 19:31:47 +0530

    Update MainActivity.java
    Instructions to import package InputMethodManager.

--- app/src/main/java/com/reallycool/harsha/buss/MainActivity.java
+++ app/src/main/java/com/reallycool/harsha/buss/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
